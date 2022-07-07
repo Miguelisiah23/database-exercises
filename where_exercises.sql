d9311e86c37fe1c5f189a6bb7104edf293863650
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-07-07 15:06:53 -0500

    WHERE exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,16 +1,16 @@
 USE employees;
 
-SELECT first_name,last_name FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
 
-SELECT first_name,last_name FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%';
 
-SELECT first_name,last_name FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE '%q%';
 
-SELECT first_name, last_name FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 
-SELECT first_name,last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 
-SELECT first_name,last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
-SELECT first_name,last_name FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
 
